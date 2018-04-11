1bd4745f22159c1ece1281b7bdae7e815480f00f
routarddev <kessetana@gmail.com>
2018-04-11 02:58:11 +0200

    Add API documentation using Swagger

--- dreams/urls.py
+++ dreams/urls.py
@@ -1,8 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework import routers
 from dreams.views import DreamViewSet
+
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Dreams API')
 
 router = routers.DefaultRouter()
 router.register(r'dreams', DreamViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    #url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^docs/', schema_view),
+]
+urlpatterns += router.urls
